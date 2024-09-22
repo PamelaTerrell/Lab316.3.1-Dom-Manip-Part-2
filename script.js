7f64a29b61a4d97817094dfa9f831af4e5c0f096
PamelaTerrell <pammyhoney@yahoo.com>
2024-09-22 15:00:44 -0400

    Part2

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 // Menu data structure
-var menuLinks = [
-    { text: 'about', href: '/about' },
-    { text: 'catalog', href: '/catalog' },
-    { text: 'orders', href: '/orders' },
-    { text: 'account', href: '/account' },
-    ];
 
+var menuLinks = [
+    {text: 'about', href: '/about'},
+    {text: 'catalog', href: '#', subLinks: [
+      {text: 'all', href: '/catalog/all'},
+      {text: 'top selling', href: '/catalog/top'},
+      {text: 'search', href: '/catalog/search'},
+    ]},
+    {text: 'orders', href: '#' , subLinks: [
+      {text: 'new', href: '/orders/new'},
+      {text: 'pending', href: '/orders/pending'},
+      {text: 'history', href: '/orders/history'},
+    ]},
+    {text: 'account', href: '#', subLinks: [
+      {text: 'profile', href: '/account/profile'},
+      {text: 'sign out', href: '/account/signout'},
+    ]},
+  ];
 
 let mainEl = document.querySelector('main')
 console.log(mainEl)
@@ -47,3 +58,18 @@ console.log(newLink)
 topMenuEl.appendChild(newLink)
 
 })
+
+let subMenuEl = document.getElementById('sub-menu')
+
+subMenuEl.style.height = '100%';
+
+subMenuEl.style.backgroundColor = 'var(--sub-menu-bg)'
+
+subMenuEl.classList.add('flex-around')
+
+let topMenuLinks = document.getElementsByName('menuLinks')
+
+topMenuEl.addEventListener("click" , function (){
+    preventDefault()
+} )
+
